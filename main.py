d0657cf6ef285ab9c8d1e758c53ef88bc6cd2737
Westy21 <westmphahlele.mw@gmail.com>
2024-03-29 14:53:25 +0200

    added comple code to generate some ascii text

---
+++ main.py
@@ -0,0 +1,512 @@
+class Letter:
+    def getLetterChar(self):
+        pass
+
+    def getLetterArray(self):
+        pass
+
+
+class A(Letter):
+    def getLetterChar(self):
+        return 'A'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/::\\:\\__\\ ",
+            "\\/\\::/  / ",
+            "  /:/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class B(Letter):
+    def getLetterChar(self):
+        return 'B'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/::\\:\\__\\ ",
+            "\\:\\::/  / ",
+            " \\::/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class C(Letter):
+    def getLetterChar(self):
+        return 'C'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/:/\\:\\__\\ ",
+            "\\:\\ \\/__/ ",
+            " \\:\\__\\   ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class D(Letter):
+    def getLetterChar(self):
+        return 'D'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/:/\\:\\__\\ ",
+            "\\:\\/:/  / ",
+            " \\::/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class E(Letter):
+    def getLetterChar(self):
+        return 'E'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/::\\:\\__\\ ",
+            "\\:\\:\/  / ",
+            " \\:\/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class F(Letter):
+    def getLetterChar(self):
+        return 'F'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/::\\:\\__\\ ",
+            "\\/\\::\/__/",
+            "   \\/__/  ",
+            "          "
+        ]
+        return arr
+
+
+class G(Letter):
+    def getLetterChar(self):
+        return 'G'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/:/\\:\\__\\ ",
+            "\\:\\:\\/__/ ",
+            " \\::/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class H(Letter):
+    def getLetterChar(self):
+        return 'H'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /:/__/_  ",
+            "/::\\/\\__\\ ",
+            "\\/\\::/  / ",
+            "  /:/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class I(Letter):
+    def getLetterChar(self):
+        return 'I'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " _\\:\\  \\  ",
+            "/\\/::\\__\\ ",
+            "\\::/\/__/ ",
+            " \\:\\__\\   ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class J(Letter):
+    def getLetterChar(self):
+        return 'J'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " _\\:\\  \\  ",
+            "/\\/::\\__\\ ",
+            "\\::/\/__/",
+            " \\/__/   ",
+            "         "
+        ]
+        return arr
+
+
+class K(Letter):
+    def getLetterChar(self):
+        return 'K'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /:/ _/_  ",
+            "/:/-\"\\__\\ ",
+            "\\;:;-\",-\" ",
+            " |:|  |   ",
+            "  \|__|   "
+        ]
+        return arr
+
+
+class L(Letter):
+    def getLetterChar(self):
+        return 'L'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /:/  /   ",
+            "/:/__/    ",
+            " \\:\\__\\   ",
+            "  \\:\\__\\  ",
+            "   \\/__/  "
+        ]
+        return arr
+
+
+class M(Letter):
+    def getLetterChar(self):
+        return 'M'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /::L_L_  ",
+            "/:/L:\__\\ ",
+            "\/_/:/  / ",
+            "  /:/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class N(Letter):
+    def getLetterChar(self):
+        return 'N'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /:| _|_  ",
+            "/::|/\\__\\ ",
+            "\\/|::/  / ",
+            "  |:/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class O(Letter):
+    def getLetterChar(self):
+        return 'O'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/:/\\:\\__\\ ",
+            "\\:\\/:/  / ",
+            " \\::/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class P(Letter):
+    def getLetterChar(self):
+        return 'P'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/:/\\:\\__\\ ",
+            "\\/\\::/  / ",
+            "   \\/__/  ",
+            "          "
+        ]
+        return arr
+
+
+class Q(Letter):
+    def getLetterChar(self):
+        return 'Q'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            " \\:\\:\\__\\",
+            "  \\::/  / ",
+            "  /:/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class R(Letter):
+    def getLetterChar(self):
+        return 'R'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/::\\:\\__\\ ",
+            "\\;:::/  / ",
+            " |:\\/__/  ",
+            "  \\|__|   "
+        ]
+        return arr
+
+
+class S(Letter):
+    def getLetterChar(self):
+        return 'S'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " /::\\  \\  ",
+            "/\\:\\:\\__\\ ",
+            "\\:\\:\\/__/ ",
+            " \\::/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+
+class T(Letter):
+    def getLetterChar(self):
+        return 'T'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            "  \\:\\  \\  ",
+            "   /::\\__\\",
+            " /:/\/__/ ",
+            " \\/__/    ",
+            "          "
+        ]
+        return arr
+
+
+class U(Letter):
+    def getLetterChar(self):
+        return 'U'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /:/ _/_  ",
+            " /:/_/\\__\\",
+            "\\:\/:/  / ",
+            " \\::/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class V(Letter):
+    def getLetterChar(self):
+        return 'V'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /:/ _/_  ",
+            " |::L/\__\\",
+            " |::::/  /",
+            "  L;;/__/ ",
+            "          "
+        ]
+        return arr
+
+
+class W(Letter):
+    def getLetterChar(self):
+        return 'W'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " /:/\\__\\  ",
+            " /:/:/\__\\",
+            "\\::/:/  / ",
+            " \\::/  /  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class X(Letter):
+    def getLetterChar(self):
+        return 'X'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " |::L__L  ",
+            " /::::\\__\\",
+            "\\;::;/__/ ",
+            " |::|__|  ",
+            "  \\/__/   "
+        ]
+        return arr
+
+
+class Y(Letter):
+    def getLetterChar(self):
+        return 'Y'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\__\\   ",
+            " |::L__L  ",
+            " |:::\\__\\ ",
+            " /:;;/__/ ",
+            " \\/__/    ",
+            "          "
+        ]
+        return arr
+
+
+class Z(Letter):
+    def getLetterChar(self):
+        return 'Z'
+
+    def getLetterArray(self):
+        arr = [
+            "   ___    ",
+            "  /\\  \\   ",
+            " _\\:\\  \\  ",
+            "/::::\__\\ ",
+            "\::;;/__/ ",
+            " \:\\__\\  ",
+            "  \\/__/  "
+        ]
+        return arr
+    
+class Space(Letter):
+    def getLetterChar(self):
+        return ' '
+
+    def getLetterArray(self):
+        arr = [
+            "       ",
+            "       ",
+            "       ",
+            "       ",
+            "       ",
+            "       ",
+            "       ",
+            "       "
+        ]
+        return arr
+
+
+def main() -> None:
+    word:str = input("Enter a word: ")
+    word = word.upper()
+
+    letters:dict[str,Letter] = {
+        'A': A(),
+        'B': B(),
+        'C': C(),
+        'D': D(),
+        'E': E(),
+        'F': F(),
+        'G': G(),
+        'H': H(),
+        'I': I(),
+        'J': J(),
+        'K': K(),
+        'L': L(),
+        'M': M(),
+        'N': N(),
+        'O': O(),
+        'P': P(),
+        'Q': Q(),
+        'R': R(),
+        'S': S(),
+        'T': T(),
+        'U': U(),
+        'V': V(),
+        'W': W(),
+        'X': X(),
+        'Y': Y(),
+        'Z': Z(),
+        ' ': Space()
+    }
+
+    for row in range(7):
+        line:str = ""
+        for char in word:
+            letter = letters.get(char)
+            line += letter.getLetterArray()[row] + " "
+        print(line)
+
+
+if __name__ == "__main__":
+    main()
